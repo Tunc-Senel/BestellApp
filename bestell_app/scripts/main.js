7b4438c94f80542b996cebe629c0ef4effd4a2ad
Tunc Senel <senel.tunc@gmail.com>
2025-12-17 16:20:13 +0100

    feat: implement dynamic basket with add, increase, decrease, remove, and update items

--- bestell_app/scripts/main.js
+++ bestell_app/scripts/main.js
@@ -1,9 +1,38 @@
 // DOM element constants
 const LIST_OF_DISHES = Array.from(document.querySelectorAll("section.menu-section article"));
+const SELECTED_ITEMS = document.querySelector(".selected-items")
+
+
+
+// Object that holds the arrays for items in the basket
+const itemsInBasket = {
+  "titles": [],
+  "prices":[],
+  "amounts": [],
+}
+
+// Array of dish names for finding indices that match the json dishes name property
+const dishNames = [
+  "Veggie mushroom black burger",
+  "All meat burger",
+  "Beef red burger",
+  "Big chicken burger",
+  "Pizza Margherita",
+  "Pizza Chorizo",
+  "Funghi",
+  "Quattro Formaggi with Chicken ",
+  "Warm beef arugula salad",
+  "Mini green Salad",
+  "Green Salad with sea food",
+  "Vegan green salad with tofu"
+]
+
+
 
 /**
  * Renders the dish descriptions into the article elements.
  * It iterates over each article element in the LIST_OF_DISHES array and sets its innerHTML.
+ * 
  */ 
 function renderDishes() {
     for (let listOfDishesIndex = 0; listOfDishesIndex < LIST_OF_DISHES.length; listOfDishesIndex++ ) {
@@ -14,30 +43,96 @@ function renderDishes() {
 /**
  * Adds an item to the basket and updates the button section.
  * @param {number} index - The index of the dish in the dishes array.
- * @param {string} amount - The property name for the amount in the dishes array.
+ * 
  */ 
-function addItemToBasket(index, amount) {
+function addItemToBasket(index) {
+  itemsInBasket.titles.push(dishes[index].name)
+  itemsInBasket.prices.push(dishes[index].price)
+  itemsInBasket.amounts.push(1)
+
+  let currenBasketIndex = itemsInBasket.titles.length - 1 
+
   const LIST_OF_ADD_ITEM_TO_BASKET_SECTION = Array.from(document.querySelectorAll(".add-item-to-basket-section"));
-  dishes[index][amount] += 1;
-  let newAmount = dishes[index][amount];
-  LIST_OF_ADD_ITEM_TO_BASKET_SECTION[index].innerHTML = getAddedButtonSectionTemplate(index, newAmount);
+  LIST_OF_ADD_ITEM_TO_BASKET_SECTION[index].innerHTML = getAddedButtonSectionTemplate(index, itemsInBasket.amounts[currenBasketIndex], currenBasketIndex);
+  SELECTED_ITEMS.insertAdjacentHTML("beforeend", getItemInBasketTemplate(index, itemsInBasket.titles[currenBasketIndex], itemsInBasket.amounts[currenBasketIndex], itemsInBasket.prices[currenBasketIndex], currenBasketIndex));
+  console.table(itemsInBasket);
 }
 
 /**
  * Adds one more item to the basket and updates the button section.
  * @param {number} index - The index of the dish in the dishes array.
- * @param {string} amount - The property name for the amount in the dishes array.
- */ 
-function increaseItemAmount(index, amount) {
-  addItemToBasket(index, amount)
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ *
+ */
+function increaseItemAmount(index, itemIndex) {
+
+  const LIST_OF_ADD_ITEM_TO_BASKET_SECTION = Array.from(document.querySelectorAll(".add-item-to-basket-section"));
+  const ARTICLE_ITEM_IN_BASKET = Array.from(document.querySelectorAll(".selected-items article"));
+
+  itemsInBasket.amounts[itemIndex]++;
+
+  ARTICLE_ITEM_IN_BASKET[itemIndex].innerHTML = getUpdateItemInBasketMinusButton(index, itemsInBasket.titles[itemIndex], itemsInBasket.amounts[itemIndex], itemsInBasket.prices[itemIndex], itemIndex)
+  LIST_OF_ADD_ITEM_TO_BASKET_SECTION[index].innerHTML = getAddedButtonSectionTemplate(index, itemsInBasket.amounts[itemIndex], itemIndex);
+  console.table(itemsInBasket);
 }
 
-function decreaseItemAmount() {
+/**
+ * Decreases the item amount in the basket and updates the button section.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ *
+ */
+function decreaseItemAmount(index, itemIndex) {
+  const LIST_OF_ADD_ITEM_TO_BASKET_SECTION = Array.from(document.querySelectorAll(".add-item-to-basket-section"));
+  const ARTICLE_ITEM_IN_BASKET = Array.from(document.querySelectorAll(".selected-items article"));
 
+  itemsInBasket.amounts[itemIndex]--;
+
+  if (itemsInBasket.amounts[itemIndex] == 1) {
+      ARTICLE_ITEM_IN_BASKET[itemIndex].innerHTML = getDefaultButtonSetupInBasket(index, itemsInBasket.titles[itemIndex], itemsInBasket.amounts[itemIndex], itemsInBasket.prices[itemIndex], itemIndex)
+  } else {
+      ARTICLE_ITEM_IN_BASKET[itemIndex].innerHTML = getUpdateItemInBasketMinusButton(index, itemsInBasket.titles[itemIndex], itemsInBasket.amounts[itemIndex], itemsInBasket.prices[itemIndex], itemIndex)
+
+  }
+  LIST_OF_ADD_ITEM_TO_BASKET_SECTION[index].innerHTML = getAddedButtonSectionTemplate(index, itemsInBasket.amounts[itemIndex], itemIndex);
+  console.table(itemsInBasket);
 }
 
-function removeItemFromBasket() {
+/**
+* Removes an item from the basket and updates the button section.
+* @param {number} index - The index of the dish in the dishes array.
+* @param {number} itemIndex - The index of the item in the basket arrays.
+*
+*/ 
+function removeItemFromBasket(index, itemIndex) {
+   const LIST_OF_ADD_ITEM_TO_BASKET_SECTION = Array.from(document.querySelectorAll(".add-item-to-basket-section"))
+    itemsInBasket.titles.splice(itemIndex, 1)
+    itemsInBasket.prices.splice(itemIndex, 1)
+    itemsInBasket.amounts.splice(itemIndex, 1)
+    LIST_OF_ADD_ITEM_TO_BASKET_SECTION[index].innerHTML = getDefaultButtonSetupInMenu(index)
+    updateBasket();
+    console.table(itemsInBasket);
+}
 
+/**
+ * Updates the basket display based on the current items in the basket.
+ *
+ */
+function updateBasket() {
+  document.querySelector(".selected-items").innerHTML = "";
+  
+  const LIST_OF_ADD_ITEM_TO_BASKET_SECTION = Array.from(document.querySelectorAll(".add-item-to-basket-section"));
+
+  for (let currenBasketIndex = 0; currenBasketIndex < itemsInBasket.titles.length; currenBasketIndex++) {
+    let currentDishIndex = dishNames.findIndex( dishName => dishName === itemsInBasket.titles[currenBasketIndex] )
+    if (itemsInBasket.amounts[currenBasketIndex] == 1) {
+        LIST_OF_ADD_ITEM_TO_BASKET_SECTION[currentDishIndex].innerHTML = getAddedButtonSectionTemplate(currentDishIndex, itemsInBasket.amounts[currenBasketIndex], currenBasketIndex);
+        SELECTED_ITEMS.insertAdjacentHTML("beforeend", getItemInBasketTemplate(currentDishIndex, itemsInBasket.titles[currenBasketIndex], itemsInBasket.amounts[currenBasketIndex], itemsInBasket.prices[currenBasketIndex], currenBasketIndex));
+    } else {
+        LIST_OF_ADD_ITEM_TO_BASKET_SECTION[currentDishIndex].innerHTML = getAddedButtonSectionTemplate(currentDishIndex, itemsInBasket.amounts[currenBasketIndex], currenBasketIndex);
+        SELECTED_ITEMS.insertAdjacentHTML("beforeend", getUpdateItemInBasketMinusButtonUpdate(currentDishIndex, itemsInBasket.titles[currenBasketIndex], itemsInBasket.amounts[currenBasketIndex], itemsInBasket.prices[currenBasketIndex], currenBasketIndex));
+    }
+  }
 }
 
 
--- bestell_app/scripts/template.js
+++ bestell_app/scripts/template.js
@@ -23,7 +23,7 @@ function getDishDescriptionTemplate(dishIndex, img, name, desc, price) {
             <div class="buy-info">
                 <span class="price">${dishes[dishIndex][price].toFixed(2).replace(".", ",")}€</span>
                 <div class="add-item-to-basket-section">
-                    <button onclick="addItemToBasket(${dishIndex}, 'amount')" class="add-item-to-basket-btn">Add to basket</button>
+                    <button onclick="addItemToBasket(${dishIndex})" class="add-item-to-basket-btn">Add to basket</button>
                 </div>
             </div>
            `
@@ -32,12 +32,164 @@ function getDishDescriptionTemplate(dishIndex, img, name, desc, price) {
 /**
  * Generates the HTML template for the added button section.
  * @param {number} index - The index of the dish in the dishes array.
- * @param {number} amount - The current amount of the dish in the basket.
+ * @param {number} amount - The amount of the specific item in the basket.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
  * @returns {string} The HTML template for the added button section.
  */
-function getAddedButtonSectionTemplate(index, amount) {
+function getAddedButtonSectionTemplate(index, amount, itemIndex) {
     return `
             <button class="added-item-to-basket-btn">Added ${amount}</button>
-            <button onclick="increaseItemAmount(${index}, 'amount')" class="item-added-increase">+</button>
+            <button onclick="increaseItemAmount(${index}, ${itemIndex})" class="item-added-increase">+</button>
+           `
+}
+
+/**
+ * Generates the HTML template for an item in the basket.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @param {string} name - The name of the dish.
+ * @param {number} amount - The amount of the specific item in the basket.
+ * @param {number} price - The price of the dish.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ * @returns {string} The HTML template for an item in the basket.
+ */
+function getItemInBasketTemplate(index, name, amount, price, itemIndex) {
+    return `
+            <article>
+                <div class="item-amount">
+                    <span>${name}</span>
+                    <div class="amount-control">
+                        <button onclick="removeItemFromBasket(${index}, ${itemIndex})" class="cart-item-remove">
+                            <img src="./src/img/favicons/delete.png" alt="">
+                        </button>
+                        <span id="itemAmountInBasket">
+                            ${amount}
+                        </span>
+                        <button onclick="increaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>+</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-price">
+                    <span>${price.toFixed(2).replace(".", ",")}€</span>
+                </div>
+            </article>
+           `
+}
+
+/**
+ * Generates the HTML template for an item in the basket.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @param {string} name - The name of the dish.
+ * @param {number} amount - The amount of the specific item in the basket.
+ * @param {number} price - The price of the dish.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ * @returns {string} The HTML template for an item in the basket.
+ */
+function getUpdateItemInBasketMinusButton(index, name, amount, price, itemIndex) {
+    return `
+                <div class="item-amount">
+                    <span>${name}</span>
+                    <div class="amount-control">
+                        <button onclick="decreaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>-</span>
+                        </button>
+                        <span id="itemAmountInBasket">
+                            ${amount}
+                        </span>
+                        <button onclick="increaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>+</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-price">
+                    <span>${price.toFixed(2).replace(".", ",")}€</span>
+                </div>
+           `
+}
+
+/**
+ * Generates the HTML template for an item in the basket.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @param {string} name - The name of the dish.
+ * @param {number} amount - The amount of the specific item in the basket.
+ * @param {number} price - The price of the dish.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ * @returns {string} The HTML template for an item in the basket.
+ */
+function getUpdateItemInBasketMinusButtonUpdate(index, name, amount, price, itemIndex) {
+    return `
+            <article>
+                <div class="item-amount">
+                    <span>${name}</span>
+                    <div class="amount-control">
+                        <button onclick="decreaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>-</span>
+                        </button>
+                        <span id="itemAmountInBasket">
+                            ${amount}
+                        </span>
+                        <button onclick="increaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>+</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-price">
+                    <span>${price.toFixed(2).replace(".", ",")}€</span>
+                </div>
+            </article>
+           `
+}
+
+/**
+ * Generates the HTML template for an item in the basket.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @param {string} name - The name of the dish.
+ * @param {number} amount - The amount of the specific item in the basket.
+ * @param {number} price - The price of the dish.
+ * @param {number} itemIndex - The index of the item in the basket arrays.
+ * @returns {string} The HTML template for an item in the basket.
+ */
+function getDefaultButtonSetupInBasket(index, name, amount, price, itemIndex) {
+    return `
+                <div class="item-amount">
+                    <span>${name}</span>
+                    <div class="amount-control">
+                        <button onclick="removeItemFromBasket(${index}, ${itemIndex})" class="cart-item-remove">
+                            <img src="./src/img/favicons/delete.png" alt="">
+                        </button>
+                        <span id="itemAmountInBasket">
+                            ${amount}
+                        </span>
+                        <button onclick="increaseItemAmount(${index}, ${itemIndex})" class="cart-item-increase-decrease">
+                            <span>+</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-price">
+                    <span>${price.toFixed(2).replace(".", ",")}€</span>
+                </div>
+           `
+}
+
+/**
+ * Generates the HTML template for the amount of the specific item in the basket.
+ * @param {number} amount - The current amount of the dish in the basket.
+ * @returns {string} The HTML template for updating the item amount in the basket.
+ */
+function getUpdateItemAmountInBasket(amount) {
+    return `
+            ${amount}
+           `
+
+}
+
+/**
+ * Generates the HTML template for the default button setup in the menu.
+ * @param {number} index - The index of the dish in the dishes array.
+ * @returns {string} The HTML template for the default button setup in the menu.
+ */
+function getDefaultButtonSetupInMenu(index) {
+    return `
+             <button onclick="addItemToBasket(${index})" class="add-item-to-basket-btn">Add to basket</button>
            `
 }
